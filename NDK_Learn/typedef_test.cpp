c722a2079756f1a5aed2f1fd9ad9fbc8985859e9
long <jiahengcen@gmail.com>
2018-05-07 10:49:57 +0800

    struct and typedef

---
+++ NDK_Learn/typedef_test.cpp
@@ -0,0 +1,33 @@
+﻿#include "stdafx.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <Windows.h> //only for system("pause")
+//测试环境
+//OS：WIN10
+//VS :VS2015
+
+
+
+/*********************typedef*********************/
+//typedef int _uint;
+//typedef char* string;
+//int main()
+//{
+//	_uint a= 20;
+//	printf("%d", a);
+//	string str = "Hello";
+//	printf("%s", str);
+//	system("pause");
+//	return 0;
+//}
+/*********************typedef 函数指针*********************/
+//
+//typedef int(*PS)(const char   *, char const  *);
+//int main()
+//{
+//	PS f;
+//	f = strcmp;
+//	
+//	system("pause");
+//	return 0;
+//}
